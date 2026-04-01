d896deed78ce7954edc68128371cd9dbda9f8723
quockhanh53197 <quockhanh53197@gmail.com>
2026-04-01 13:35:55 +0700

    Create SJF.cpp

---
+++ Code/SJF.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <limits>
+#include <string>
+
+using namespace std;
+class Process {
+public:
+    int pid;
+    int arrival;
+    int burst;
+    int waiting;
+    int turnaround;
+    int completion;
+
+    Process(int id = 0, int a = 0, int b = 0) 
+        : pid(id), arrival(a), burst(b), waiting(0), turnaround(0), completion(0) {}
+};
+
+class InputHandler {
+public:
+    static bool readInt(int& x) {
+        cin >> x;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return false;
+        }
+        return true;
+    }
+};
+
+class Scheduler {
+protected:
+    vector<Process> processes;
+    double totalWT;
+    double totalTAT;
+    string algoName;
+
+public:
+    Scheduler(string name, const vector<Process>& p) 
+        : algoName(name), processes(p), totalWT(0), totalTAT(0) {}
+    virtual ~Scheduler() = default;
+
+    virtual void execute() = 0;
+
+    void display() const {
+        cout << "\n===== " << algoName << " =====\n";
+        cout << left << setw(10) << "PID"
+             << setw(15) << "Arrival"
+             << setw(15) << "Burst"
+             << setw(15) << "Completion"
+             << setw(15) << "Turnaround"
+             << setw(15) << "Waiting" << "\n";
+
+        for (const auto& p : processes) {
+            cout << left << setw(10) << p.pid
+                 << setw(15) << p.arrival
+                 << setw(15) << p.burst
+                 << setw(15) << p.completion
+                 << setw(15) << p.turnaround
+                 << setw(15) << p.waiting << "\n";
+        }
+
+        cout << fixed << setprecision(2);
+        if (!processes.empty()) {
+            cout << "Average WT: " << totalWT / processes.size() << endl;
+            cout << "Average TAT: " << totalTAT / processes.size() << endl;
+        }
+    }
+};
