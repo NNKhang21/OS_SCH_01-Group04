49db6a5a3b0adef7152bbf20e7e510e9a64d20d6
Nguyễn Nguyên Khang <kn537190@gmail.com>
2026-04-10 21:55:22 +0700

    Delete Code/widget.cpp

--- Code/widget.cpp
+++
@@ -1,7 +0,0 @@
-#include "widget.h"
-#include "ui_widget.h"
-#include "Scheduler.h" // Import code thuật toán của bạn
-#include <vector>
-
-std::vector<Process> processList;
-int currentPid = 1; // Biến đếm PID tự động tăng
